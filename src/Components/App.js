233fdef23ae67ff7dc6a9422e3837043c96a5c87
“Saki <sakisandra.c@gmail.com>
2023-06-23 10:59:06 -0600

    Add random spell to hompage

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Homepage from './Homepage/Homepage';
 import Login from './Login/Login';
+import Navigation from './Navigation/Navigation';
 
 function App() {
   return (
     <>
+    <Navigation/>
     <Routes>
       <Route path="/" element={<Homepage/>}></Route>
       <Route path="/login" element={<Login/>}></Route>
--- src/Components/Homepage/Homepage.js
+++ src/Components/Homepage/Homepage.js
@@ -1,35 +1,92 @@
-import React, { useState } from 'react'
-import './Homepage.css'
+import React, { useEffect, useState } from 'react';
+import './Homepage.css';
+import RandomSpell from '../RandomSpell/RandomSpell';
 
 const Homepage = () => {
-  const [user, setUser] = useState({name: "Wizard", house:""})
+
+  //STATE
+  const [user, setUser] = useState({
+    name: "Wizard",
+    house:"",
+    savedSpells: [],
+    clicks: 0
+  });
+  const [allSpells, setAllSpells] = useState([]);
+  const [randomSpell, setRandomSpell] = useState([]);
+
+  //USE EFFECT
+  useEffect(() => {
+    fetch('https://wizard-world-api.herokuapp.com/Spells')
+    .then(res => res.json())
+    .then(data => setAllSpells(data))
+  },[])
+
+  useEffect(() => {
+    setRandomSpell(allSpells[getRandomIndex(allSpells)])
+    console.log(allSpells)
+    console.log(randomSpell)
+  }, [user.clicks])
 
   const selectHouse = (e) => {
-    const {name, value} = e.target;
+  const {name, value} = e.target;
 
     setUser(prev => {
       return {...prev, [name]: value}
+    });
+  }
+
+  const getRandomIndex = (allSpells) => {
+    return Math.floor(Math.random() * allSpells.length)
+  }
+
+  const saveSpell = () => {
+    const saved = user.savedSpells.some(spell => {
+      return spell.id === randomSpell.id
     })
 
+    if (!saved) {
+      setUser(prev => {
+        return {
+          ...prev,
+          savedSpells: [...prev.savedSpells, randomSpell]
+        }
+      })
+    }
+    console.log(user)
+  }
+
+  const getRandomSpell = () => {
+    setUser(prev => {
+      return {...prev, clicks: prev.clicks + 1}
+    })
   }
 
   return (
+    <>
     <div className={`main-container ${user.house ? user.house : ""}`}>
-      <div>
+      <main className="homepage-container">
         <h1>{`Hello ${user.name}`}</h1>
-        <select
-        id="house"
-        value={user.house}
-        onChange={selectHouse}
-        name="house">
+        <label className="house-label" htmlFor="house">Choose Your House:</label>
+        <select 
+          className="house-selector"
+          id="house"
+          value={user.house}
+          onChange={selectHouse}
+          name="house">
           <option value="">---</option>
           <option value="gryffindor">Gryffindor</option>
           <option value="ravenclaw">Ravenclaw</option>
           <option value="hufflepuff">Hufflepuff</option>
           <option value="slytherin">Slytherin</option>
         </select>
-      </div>
+      </main>
     </div>
+    <div className="random-spell-container">
+      <RandomSpell randomSpell={randomSpell} />
+    </div>
+    <button className='save-btn' onClick={saveSpell}>Save Spell</button>
+    <button className='save-btn' onClick={getRandomSpell}>Get Random Spell</button>
+    </>
   )
 }
 
